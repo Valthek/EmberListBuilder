713df0a8f03654106d698f6463b72d5851ce13c8
Sebastiaan Vandekerkhove <svandekerkhove@socmut.be>
2023-02-10 16:11:45 +0100

    Setting up basic component structure

---
+++ EmberListBuilder/Pages/Components/MapBuilder.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EmberListBuilder.Pages.Components
+{
+    public partial class MapBuilder : ComponentBase
+    {
+
+    }
+}
---
+++ EmberListBuilder/Pages/Components/PartDisplay.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EmberListBuilder.Pages.Components
+{
+    public partial class PartDisplay : ComponentBase
+    {
+        public PartData PartData { get; set; }
+
+        public EditPart()
+        {
+
+        }
+    }
+}
